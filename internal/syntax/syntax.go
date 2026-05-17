24a8adb7fb41d1840f130e382b3c02c3a642819e
Tom Fleet <me@followtheprocess.codes>
2026-05-17 20:49:52 +0100

    Add some TODOs and an uncovered test case

--- internal/syntax/syntax.go
+++ internal/syntax/syntax.go
@@ -14,3 +14,9 @@ package syntax
 // A good compromise might be accepting a continuous run of characters as Text
 // without quotes so like `https://thisis/all/astring` but as soon as it contains
 // whitespace it needs to be wrapped in quotes?
+
+// TODO(@FollowTheProcess): Syntax gaps:
+// - URL continuation, indented newlines after the request line with url content on them
+// - HTTPVersion on the request line, do we need a HTTPVersion token?
+// - Multi-arg directives like `@prompt id "The user's ID"` and `@secret password "Super secret shh"`
+// - Add a state in the block dispatch for <>, >>, or > {% %} after a body
