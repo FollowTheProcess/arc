136731e078fb74879511ce7f96049eafcdc2cb0d
Tom Fleet <me@followtheprocess.codes>
2026-05-25 12:01:58 +0100

    Reformat ast

--- internal/syntax/ast/expression.go
+++ internal/syntax/ast/expression.go
@@ -2,27 +2,23 @@ package ast
 
 import "go.followtheprocess.codes/arc/internal/syntax/source"
 
-// Ident is a named identifier expression.
-type Ident struct {
-	Name string // The name of the ident
-	Span source.Span
-}
+type (
+	// Ident is a named identifier expression.
+	Ident struct {
+		Name string // The name of the ident
+		Span source.Span
+	}
 
-// Pos returns the span for the ident.
-func (i Ident) Pos() source.Span {
-	return i.Span
-}
+	// TextLiteral is an (optionally quoted) string of text.
+	TextLiteral struct {
+		Value string // The (unquoted) text value
+		Span  source.Span
+	}
+)
 
-// TextLiteral is an, optionally quoted, string of text.
-type TextLiteral struct {
-	Value string // The (unquoted) text value
-	Span  source.Span
-}
-
-// Pos returns the span for the text.
-func (t TextLiteral) Pos() source.Span {
-	return t.Span
-}
+// Pos implementations.
+func (i Ident) Pos() source.Span       { return i.Span }
+func (t TextLiteral) Pos() source.Span { return t.Span }
 
 // Expression implementations.
 func (i Ident) expressionNode()       {}
--- internal/syntax/ast/statement.go
+++ internal/syntax/ast/statement.go
@@ -2,29 +2,25 @@ package ast
 
 import "go.followtheprocess.codes/arc/internal/syntax/source"
 
-// File is the AST node representing a single .http file.
-type File struct {
-	Span       source.Span
-	Statements []Statement
-}
+type (
+	// File is the AST node representing a single .http file.
+	File struct {
+		Statements []Statement
+		Span       source.Span
+	}
 
-// Pos returns the span for the file.
-func (f File) Pos() source.Span {
-	return f.Span
-}
+	// Directive is the AST node representing a directive e.g.
+	// `@var id = 123` or `@no-redirect`.
+	Directive struct {
+		Value Expression
+		Ident Ident
+		Span  source.Span
+	}
+)
 
-// Directive is the AST node representing a directive e.g.
-// `@var id = 123` or `@no-redirect`.
-type Directive struct {
-	Value Expression
-	Ident Ident
-	Span  source.Span
-}
-
-// Pos returns the span for the directive.
-func (d Directive) Pos() source.Span {
-	return d.Span
-}
+// Pos implementations.
+func (f File) Pos() source.Span      { return f.Span }
+func (d Directive) Pos() source.Span { return d.Span }
 
 // Statement implementations.
 func (f File) statementNode()      {}
