c99993451f7e17a45641b1ab9825f08f25ff2f4c
Tom Fleet <me@followtheprocess.codes>
2026-06-04 08:05:52 +0100

    Allow comments in requests

--- internal/syntax/assembler/assembler.go
+++ internal/syntax/assembler/assembler.go
@@ -176,7 +176,7 @@ func assembleRequest(blocks []block.Block) (ast.Request, []diagnostic.Diagnostic
 			req.Headers = append(req.Headers, p.parseHeader())
 		case block.Body:
 			req.Body = p.parseBody()
-		case block.Blank:
+		case block.Blank, block.Comment:
 			// Nothing
 		default:
 			d := diagnostic.Error(fmt.Sprintf("unexpected block type: %s", b.Kind), b.Span)
